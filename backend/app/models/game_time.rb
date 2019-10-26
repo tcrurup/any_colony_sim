af0ff5fb1c635b9d61196fce7b259a4e7deb7250
tcrurup <tcrurup@gmail.com>
2019-10-25 23:21:17 -0700

    add method to advance time

--- backend/app/models/game_time.rb
+++ backend/app/models/game_time.rb
@@ -8,6 +8,29 @@ class GameTime < ApplicationRecord
 
     #VALIDATIONS
 
+    def advance_time
+        case self.time_of_day
+        when "morning"
+            self.time_of_day = 'noon'
+        when "noon"
+            self.time_of_day = 'evening'
+        when "evening"
+            self.time_of_day = 'midnight'
+        when "midnight"
+            next_day
+        end
+        self
+    end
+    
+    private
+
+    def next_day
+        self.tap do |x|
+            x.time_of_day = 'morning'
+            x.day = self.day + 1
+        end
+    end
+    
     def set_initial_day_and_time
         self.day = 1;
         self.time_of_day = 'morning'
