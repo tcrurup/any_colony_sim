66ed4d0210d15d9c215a40c9aa9646d564bb7488
tcrurup <tcrurup@gmail.com>
2019-10-26 11:27:32 -0700

    Associate a colony id and commons id on worker create

--- backend/app/models/commons.rb
+++ backend/app/models/commons.rb
@@ -11,8 +11,12 @@ class Commons < ApplicationRecord
     #FUNCTIONS
     
 
+    def colony_id
+        self.colony.id
+    end
+
     def create_worker
-        self.workers << Worker.create()
+        self.workers << Worker.create(colony_id: self.colony_id, commons_id: self.id)
     end
 
     private 
